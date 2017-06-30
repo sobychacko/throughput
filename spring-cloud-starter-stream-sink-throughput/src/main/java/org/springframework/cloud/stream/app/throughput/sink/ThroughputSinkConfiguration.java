587b9cdab73c557dca6a29008590eecec46d06b5
Soby Chacko <schacko@pivotal.io>
2017-06-30 17:00:02 -0400

    perf changes

--- spring-cloud-starter-stream-sink-throughput/src/main/java/org/springframework/cloud/stream/app/throughput/sink/ThroughputSinkConfiguration.java
+++ spring-cloud-starter-stream-sink-throughput/src/main/java/org/springframework/cloud/stream/app/throughput/sink/ThroughputSinkConfiguration.java
@@ -26,10 +26,11 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.context.Lifecycle;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.messaging.Message;
+import org.springframework.context.SmartLifecycle;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StopWatch;
 
 /**
  * A simple handler that will count messages and log witnessed throughput at some interval.
@@ -40,7 +41,9 @@ import org.springframework.messaging.Message;
  */
 @EnableBinding(Sink.class)
 @EnableConfigurationProperties({ThroughputSinkProperties.class})
-public class ThroughputSinkConfiguration implements Lifecycle {
+//@EnableKafka
+@Configuration
+public class ThroughputSinkConfiguration implements SmartLifecycle {
 
 	private final Log logger = LogFactory.getLog(getClass());
 
@@ -80,17 +83,36 @@ public class ThroughputSinkConfiguration implements Lifecycle {
 		return running;
 	}
 
-	@ServiceActivator(inputChannel=Sink.INPUT)
-	public void throughputSink(Message<?> message ) {
+	@Override
+	public boolean isAutoStartup() {
+		return true;
+	}
+
+	@Override
+	public void stop(Runnable callback) {
+		stop();
+	}
+
+	@Override
+	public int getPhase() {
+		return 0;
+	}
+
+	@StreamListener(Sink.INPUT)
+	//@KafkaListener(topics = "perf-test-1", id = "jackson-storm")
+	public void throughputSink(Object payload) {
 		if (start.get() == -1L) {
 			synchronized (start) {
 				if (start.get() == -1L) {
 					// assume a homogeneous message structure - this is intended for
 					// performance tests so we can assume that the messages are similar;
 					// therefore we'll do our reporting based on the first message
-					Object payload = message.getPayload();
-					if (payload instanceof byte[] || payload instanceof String) {
-						reportBytes = true;
+					//Object payload = message.getPayload();
+					if (payload instanceof byte[]) {
+						reportBytes = false;
+					}
+					else {
+						System.out.println("Payload is a " + payload.getClass().getName());
 					}
 					start.set(System.currentTimeMillis());
 					executorService.execute(new ReportStats());
@@ -98,58 +120,87 @@ public class ThroughputSinkConfiguration implements Lifecycle {
 			}
 		}
 		intermediateCounter.incrementAndGet();
-		if (reportBytes) {
-			Object payload = message.getPayload();
- 			if (payload instanceof byte[]) {
-				intermediateBytes.addAndGet(((byte[]) payload).length);
-			}
-			else if (payload instanceof String) {
-				intermediateBytes.addAndGet((((String) payload).getBytes()).length);
-			}
-		}
+//		if (reportBytes) {
+//			//Object payload = message.getPayload();
+// 			if (payload instanceof byte[]) {
+//				intermediateBytes.addAndGet(((byte[]) payload).length);
+//			}
+////			else if (payload instanceof String) {
+////				intermediateBytes.addAndGet((((String) payload).getBytes()).length);
+////			}
+//		}
 	}
 
 	private class ReportStats implements Runnable {
+
+		StopWatch watch = new StopWatch("foobar");
+
 		@Override
 		public void run() {
-			int reportEveryMs = properties.getReportEveryMs();
-			while (isRunning()) {
-				long intervalStart = System.currentTimeMillis();
-				try {
-					Thread.sleep(reportEveryMs);
-					long timeNow = System.currentTimeMillis();
-					long currentCounter = intermediateCounter.getAndSet(0L);
-					long currentBytes = intermediateBytes.getAndSet(0L);
-					long totalCounter = counter.addAndGet(currentCounter);
-					long totalBytes = bytes.addAndGet(currentBytes);
 
-					logger.info(
-							String.format("Messages: %10d in %5.2f%s = %11.2f/s",
-									currentCounter,
-									(timeNow - intervalStart)/ 1000.0, timeUnit, ((double) currentCounter * 1000 / reportEveryMs)));
-					logger.info(
-							String.format("Messages: %10d in %5.2f%s = %11.2f/s",
-									totalCounter, (timeNow - start.get()) / 1000.0, timeUnit,
-									((double) totalCounter * 1000 / (timeNow - start.get()))));
-					if (reportBytes) {
-						logger.info(
-								String.format("Throughput: %12d in %5.2f%s = %11.2fMB/s, ",
-										currentBytes,
-										(timeNow - intervalStart)/ 1000.0, timeUnit,
-										((currentBytes / (1024.0 * 1024)) * 1000 / reportEveryMs)));
-						logger.info(
-								String.format("Throughput: %12d in %5.2f%s = %11.2fMB/s",
-										totalBytes, (timeNow - start.get()) / 1000.0, timeUnit,
-										((totalBytes / (1024.0 * 1024)) * 1000 / (timeNow - start.get()))));
-					}
+
+			while (isRunning()) {
+				if (!watch.isRunning()) {
+					watch.start();
+				}
+
+				long currentCounter = intermediateCounter.getAndSet(0L);
+				long totalCounter = counter.addAndGet(currentCounter);
+				if (totalCounter == 30_000_000) {
+					this.watch.stop();
+					System.out.println(this.watch.toString() + "... "
+							+ (30_000_000 / ((float) watch.getTotalTimeMillis() / (float) 1000))
+							+ " messages per second");
+				}
+				try {
+					Thread.sleep(1000);
 				}
 				catch (InterruptedException e) {
-					Thread.currentThread().interrupt();
-					logger.warn("Thread interrupted", e);
-					return;
+					e.printStackTrace();
 				}
-			}
-		}
-	}
 
+			}
+
+
+//			int reportEveryMs = properties.getReportEveryMs();
+//			while (isRunning()) {
+//				long intervalStart = System.currentTimeMillis();
+//				try {
+//					Thread.sleep(reportEveryMs);
+//					long timeNow = System.currentTimeMillis();
+//					long currentCounter = intermediateCounter.getAndSet(0L);
+//					long currentBytes = intermediateBytes.getAndSet(0L);
+//					long totalCounter = counter.addAndGet(currentCounter);
+//					long totalBytes = bytes.addAndGet(currentBytes);
+//
+//					logger.info(
+//							String.format("Messages: %10d in %5.2f%s = %11.2f/s",
+//									currentCounter,
+//									(timeNow - intervalStart)/ 1000.0, timeUnit, ((double) currentCounter * 1000 / reportEveryMs)));
+//					logger.info(
+//							String.format("Messages: %10d in %5.2f%s = %11.2f/s",
+//									totalCounter, (timeNow - start.get()) / 1000.0, timeUnit,
+//									((double) totalCounter * 1000 / (timeNow - start.get()))));
+//					if (reportBytes) {
+//						logger.info(
+//								String.format("Throughput: %12d in %5.2f%s = %11.2fMB/s, ",
+//										currentBytes,
+//										(timeNow - intervalStart)/ 1000.0, timeUnit,
+//										((currentBytes / (1024.0 * 1024)) * 1000 / reportEveryMs)));
+//						logger.info(
+//								String.format("Throughput: %12d in %5.2f%s = %11.2fMB/s",
+//										totalBytes, (timeNow - start.get()) / 1000.0, timeUnit,
+//										((totalBytes / (1024.0 * 1024)) * 1000 / (timeNow - start.get()))));
+//					}
+//				}
+//				catch (InterruptedException e) {
+//					Thread.currentThread().interrupt();
+//					logger.warn("Thread interrupted", e);
+//					return;
+//				}
+//			}
+//		}
+		}
+
+	}
 }
